4d124d763280b254c37c6ac9f396036b281460ba
Daniele Irsuti <irsooti@gmail.com>
2020-03-03 20:14:20 +0100

    env

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -4,9 +4,21 @@ function getFirebaseConfig() {
   try {
     return require(`../${process.env.NODE_ENV}.firebase.config.json`);
   } catch (err) {
-    throw new Error(
-      'Missing configuration file! Remember to include your config file in the root folder: [ENVIRONMENT].firebase.config.json',
-    );
+    try {
+      return {
+        apiKey: process.env.apiKey,
+        authDomain: process.env.authDomain,
+        databaseURL: process.env.databaseURL,
+        projectId: process.env.projectId,
+        storageBucket: process.env.storageBucket,
+        messagingSenderId: process.env.messagingSenderId,
+        appId: process.env.appId,
+      };
+    } catch (subErr) {
+      throw new Error(
+        'Missing configuration file! Remember to include your config file in the root folder: [ENVIRONMENT].firebase.config.json',
+      );
+    }
   }
 }
 
